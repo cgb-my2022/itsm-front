dfdfc4e77ee4cf3b71f3a73b5b94ddd63aff4ff0
薛锋 <xuefeng@baoshugou.com>
2020-08-13 17:04:34 +0800

    登陆页面增加企业微信登录

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -45,8 +45,27 @@
               <img v-else style="margin-top: 2px;" src="../../assets/checkcode.png" @click="handleChangeCheckCode"/>
             </a-col>
           </a-row>-->
+          <!--<a-form-item>
+            <a-checkbox v-decorator="['rememberMe', {initialValue: true, valuePropName: 'checked'}]" >自动登陆</a-checkbox>
+            <router-link :to="{ name: 'alteration'}" class="forge-password" style="float: right;">
+              忘记密码
+            </router-link>
+            &lt;!&ndash;<router-link :to="{ name: 'register'}" class="forge-password" style="float: right;margin-right: 10px" >
+              注册账户
+            </router-link>&ndash;&gt;
+          </a-form-item>-->
 
-
+          <a-form-item style="margin-top:45px">
+            <a-button
+              size="large"
+              type="primary"
+              htmlType="submit"
+              class="login-button"
+              :loading="loginBtn"
+              @click.stop.prevent="handleSubmit"
+              :disabled="loginBtn">确定
+            </a-button>
+          </a-form-item>
         </a-tab-pane>
         <!--<a-tab-pane key="tab2" tab="手机号登陆">
           <a-form-item>
@@ -81,31 +100,14 @@
             </a-col>
           </a-row>
         </a-tab-pane>-->
+        <a-tab-pane :forceRender="true" key="tab2" tab="企业微信登陆">
+        <div id="wx_qrcode" class="wx_bg"></div>
+      </a-tab-pane>
+        <!--<a-tab-pane key="tab2" tab="企业微信登陆">-->
+          <!--<div class="wx_bg">企业微信登陆</div>-->
+        <!--</a-tab-pane>-->
       </a-tabs>
-
-      <a-form-item>
-        <a-checkbox v-decorator="['rememberMe', {initialValue: true, valuePropName: 'checked'}]" >自动登陆</a-checkbox>
-        <router-link :to="{ name: 'alteration'}" class="forge-password" style="float: right;">
-          忘记密码
-        </router-link>
-       <!--<router-link :to="{ name: 'register'}" class="forge-password" style="float: right;margin-right: 10px" >
-          注册账户
-        </router-link>-->
-      </a-form-item>
-
-      <a-form-item style="margin-top:24px">
-        <a-button
-          size="large"
-          type="primary"
-          htmlType="submit"
-          class="login-button"
-          :loading="loginBtn"
-          @click.stop.prevent="handleSubmit"
-          :disabled="loginBtn">确定
-        </a-button>
-      </a-form-item>
-
-      <!--<div class="user-login-other">
+     <!-- <div class="user-login-other">
         <span>其他登陆方式</span>
         <a @click="onThirdLogin('github')" title="github"><a-icon class="item-icon" type="github"></a-icon></a>
         <a @click="onThirdLogin('wechat_enterprise')" title="企业微信"><a-icon class="item-icon" type="wechat"></a-icon></a>
@@ -154,25 +156,22 @@
         </a-form-item>
       </a-form>
 
-
-
     </a-modal>
 
   </div>
 </template>
 
 <script>
-  //import md5 from "md5"
+  // import md5 from "md5"
   import api from '@/api'
   import TwoStepCaptcha from '@/components/tools/TwoStepCaptcha'
-  import { mapActions } from "vuex"
-  import { timeFix } from "@/utils/util"
+  import { mapActions } from 'vuex'
+  import { timeFix } from '@/utils/util'
   import Vue from 'vue'
-  import { ACCESS_TOKEN ,ENCRYPTED_STRING} from "@/store/mutation-types"
-  import { putAction,postAction,getAction } from '@/api/manage'
-  import { encryption , getEncryptedString } from '@/utils/encryption/aesEncrypt'
+  import { ACCESS_TOKEN, ENCRYPTED_STRING, USER_INFO } from '@/store/mutation-types'
+  import { putAction, postAction, getAction } from '@/api/manage'
+  import { encryption, getEncryptedString } from '@/utils/encryption/aesEncrypt'
   import store from '@/store/'
-  import { USER_INFO } from "@/store/mutation-types"
 
   export default {
     components: {
@@ -180,41 +179,47 @@
     },
     data () {
       return {
-        customActiveKey: "tab1",
+        customActiveKey: 'tab1',
         loginBtn: false,
         // login type: 0 email, 1 username, 2 telephone
         loginType: 0,
         requiredTwoStepCaptcha: false,
         stepCaptchaVisible: false,
         form: this.$form.createForm(this),
-        encryptedString:{
-          key:"",
-          iv:"",
+        encryptedString: {
+          key: '',
+          iv: ''
         },
         state: {
           time: 60,
-          smsSendBtn: false,
+          smsSendBtn: false
         },
-        validatorRules:{
-          username:{rules: [{ required: true, message: '请输入用户名!'},{validator: this.handleUsernameOrEmail}]},
-          password:{rules: [{ required: true, message: '请输入密码!',validator: 'click'}]},
-          mobile:{rules: [{validator:this.validateMobile}]},
-          captcha:{rule: [{ required: true, message: '请输入验证码!'}]},
-          inputCode:{rules: [{ required: true, message: '请输入验证码!'}]}
+        validatorRules: {
+          username: { rules: [{ required: true, message: '请输入用户名!' }, { validator: this.handleUsernameOrEmail }] },
+          password: { rules: [{ required: true, message: '请输入密码!', validator: 'click' }] },
+          mobile: { rules: [{ validator: this.validateMobile }] },
+          captcha: { rule: [{ required: true, message: '请输入验证码!' }] },
+          inputCode: { rules: [{ required: true, message: '请输入验证码!' }] }
         },
-        verifiedCode:"",
-        inputCodeContent:"",
-        inputCodeNull:true,
+        verifiedCode: '',
+        inputCodeContent: '',
+        inputCodeNull: true,
 
-        departList:[],
-        departVisible:false,
-        departSelected:"",
-        currentUsername:"",
-        validate_status:"",
-        currdatetime:'',
-        randCodeImage:'',
-        requestCodeSuccess:false
+        departList: [],
+        departVisible: false,
+        departSelected: '',
+        currentUsername: '',
+        validate_status: '',
+        currdatetime: '',
+        randCodeImage: '',
+        requestCodeSuccess: false
       }
+    },
+    beforeCreate () {
+      document.querySelector('body').setAttribute('style', 'overflow:hidden')
+    },
+    beforeDestroy () {
+      document.querySelector('body').removeAttribute('style')
     },
     created () {
       this.currdatetime = new Date().getTime();
@@ -222,31 +227,31 @@
       this.getRouterData();
       this.handleChangeCheckCode();
       // update-begin- --- author:scott ------ date:20190805 ---- for:密码加密逻辑暂时注释掉，有点问题
-      //this.getEncrypte();
+      // this.getEncrypte();
       // update-end- --- author:scott ------ date:20190805 ---- for:密码加密逻辑暂时注释掉，有点问题
     },
     methods: {
-      ...mapActions([ "Login", "Logout","PhoneLogin","ThirdLogin" ]),
-      //第三方登录
-      onThirdLogin(source){
-        let url = window._CONFIG['domianURL']+`/thirdLogin/render/${source}`
+      ...mapActions([ 'Login', 'Logout', 'PhoneLogin', 'ThirdLogin' ]),
+      // 第三方登录
+      onThirdLogin(source) {
+        let url = window._CONFIG['domianURL'] + `/thirdLogin/render/${source}`
         window.open(url, `login ${source}`, 'height=500, width=500, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=n o, status=no')
         let that = this;
-        let receiveMessage = function(event){
+        let receiveMessage = function(event) {
           var origin = event.origin
-          console.log("origin",origin);
+          console.log('origin', origin);
 
           let token = event.data
-          console.log("event.data",token)
-          that.ThirdLogin(token).then(res=>{
-            if(res.success){
+          console.log('event.data', token)
+          that.ThirdLogin(token).then(res => {
+            if (res.success) {
               that.loginSuccess()
-            }else{
+            } else {
               that.requestFailed(res);
             }
           })
         }
-        window.addEventListener("message", receiveMessage, false);
+        window.addEventListener('message', receiveMessage, false);
       },
       // handler
       handleUsernameOrEmail (rule, value, callback) {
@@ -268,26 +273,24 @@
         that.loginBtn = true;
         // 使用账户密码登陆
         if (that.customActiveKey === 'tab1') {
-          that.form.validateFields([ 'username', 'password','inputCode', 'rememberMe' ], { force: true }, (err, values) => {
+          that.form.validateFields([ 'username', 'password', 'inputCode', 'rememberMe' ], { force: true }, (err, values) => {
             if (!err) {
               loginParams.username = values.username
               // update-begin- --- author:scott ------ date:20190805 ---- for:密码加密逻辑暂时注释掉，有点问题
-              //loginParams.password = md5(values.password)
-              //loginParams.password = encryption(values.password,that.encryptedString.key,that.encryptedString.iv)
+              // loginParams.password = md5(values.password)
+              // loginParams.password = encryption(values.password,that.encryptedString.key,that.encryptedString.iv)
               loginParams.password = values.password
               loginParams.remember_me = values.rememberMe
               // update-begin- --- author:scott ------ date:20190805 ---- for:密码加密逻辑暂时注释掉，有点问题
               loginParams.captcha = that.inputCodeContent
               loginParams.checkKey = that.currdatetime
-              console.log("登录参数",loginParams)
+              console.log('登录参数', loginParams)
               that.Login(loginParams).then((res) => {
                 this.departConfirm(res)
               }).catch((err) => {
                 that.requestFailed(err);
               });
-
-
-            }else {
+            } else {
               that.loginBtn = false;
             }
           })
@@ -301,10 +304,10 @@
               that.PhoneLogin(loginParams).then((res) => {
                 console.log(res.result);
                 this.departConfirm(res)
+                this.mounted();
               }).catch((err) => {
                 that.requestFailed(err);
               })
-
             }
           })
         }
@@ -312,10 +315,10 @@
       getCaptcha (e) {
         e.preventDefault();
         let that = this;
-        this.form.validateFields([ 'mobile' ], { force: true },(err,values) => {
-            if(!values.mobile){
-              that.cmsFailed("请输入手机号");
-            }else if (!err) {
+        this.form.validateFields([ 'mobile' ], { force: true }, (err, values) => {
+            if (!values.mobile) {
+              that.cmsFailed('请输入手机号');
+            } else if (!err) {
               this.state.smsSendBtn = true;
               let interval = window.setInterval(() => {
                 if (that.state.time-- <= 0) {
@@ -327,11 +330,11 @@
 
               const hide = this.$message.loading('验证码发送中..', 0);
               let smsParams = {};
-                  smsParams.mobile=values.mobile;
-                  smsParams.smsmode="0";
-              postAction("/sys/sms",smsParams)
+                  smsParams.mobile = values.mobile;
+                  smsParams.smsmode = '0';
+              postAction('/sys/sms', smsParams)
                 .then(res => {
-                  if(!res.success){
+                  if (!res.success) {
                     setTimeout(hide, 0);
                     this.cmsFailed(res.message);
                   }
@@ -358,126 +361,125 @@
           this.stepCaptchaVisible = false
         })
       },
-      handleChangeCheckCode(){
+      handleChangeCheckCode() {
         this.currdatetime = new Date().getTime();
-        getAction(`/sys/randomImage/${this.currdatetime}`).then(res=>{
-          if(res.success){
+        getAction(`/sys/randomImage/${this.currdatetime}`).then(res => {
+          if (res.success) {
             this.randCodeImage = res.result
-            this.requestCodeSuccess=true
-          }else{
+            this.requestCodeSuccess = true
+          } else {
             this.$message.error(res.message)
-            this.requestCodeSuccess=false
+            this.requestCodeSuccess = false
           }
-        }).catch(()=>{
-          this.requestCodeSuccess=false
+        }).catch(() => {
+          this.requestCodeSuccess = false
         })
       },
       loginSuccess () {
         // update-begin- author:sunjianlei --- date:20190812 --- for: 登录成功后不解除禁用按钮，防止多次点击
         // this.loginBtn = false
         // update-end- author:sunjianlei --- date:20190812 --- for: 登录成功后不解除禁用按钮，防止多次点击
-        this.$router.push({ path: "/dashboard/analysis" })
+        this.$router.push({ path: '/dashboard/analysis' })
         this.$notification.success({
           message: '欢迎',
-          description: `${timeFix()}，欢迎回来`,
+          description: `${timeFix()}，欢迎回来`
         });
       },
-      cmsFailed(err){
+      cmsFailed(err) {
         this.$notification[ 'error' ]({
-          message: "登录失败",
-          description:err,
-          duration: 4,
+          message: '登录失败',
+          description: err,
+          duration: 4
         });
       },
       requestFailed (err) {
         this.$notification[ 'error' ]({
           message: '登录失败',
-          description: ((err.response || {}).data || {}).message || err.message || "请求出现错误，请稍后再试",
-          duration: 4,
+          description: ((err.response || {}).data || {}).message || err.message || '请求出现错误，请稍后再试',
+          duration: 4
         });
         this.loginBtn = false;
       },
-      validateMobile(rule,value,callback){
-        if (!value || new RegExp(/^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/).test(value)){
+      validateMobile(rule, value, callback) {
+        if (!value || new RegExp(/^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/).test(value)) {
           callback();
-        }else{
-          callback("您的手机号码格式不正确!");
-        }
-
-      },
-      validateInputCode(rule,value,callback){
-        if(!value || this.verifiedCode==this.inputCodeContent){
-          callback();
-        }else{
-          callback("您输入的验证码不正确!");
+        } else {
+          callback('您的手机号码格式不正确!');
         }
       },
-      generateCode(value){
+      validateInputCode(rule, value, callback) {
+        if (!value || this.verifiedCode == this.inputCodeContent) {
+          callback();
+        } else {
+          callback('您输入的验证码不正确!');
+        }
+      },
+      generateCode(value) {
         this.verifiedCode = value.toLowerCase()
       },
-      inputCodeChange(e){
+      inputCodeChange(e) {
         this.inputCodeContent = e.target.value
       },
-      departConfirm(res){
-        if(res.success){
+      departConfirm(res) {
+        if (res.success) {
           let multi_depart = res.result.multi_depart
-          //0:无部门 1:一个部门 2:多个部门
-          if(multi_depart==0){
+          // 0:无部门 1:一个部门 2:多个部门
+          if (multi_depart == 0) {
             this.loginSuccess()
             this.$notification.warn({
               message: '提示',
               description: `您尚未归属部门,请确认账号信息`,
-              duration:3
+              duration: 3
             });
-          }else if(multi_depart==2){
-            this.departVisible=true
-            this.currentUsername=this.form.getFieldValue("username")
+          } else if (multi_depart == 2) {
+            this.departVisible = true
+            this.currentUsername = this.form.getFieldValue('username')
             this.departList = res.result.departs
-          }else {
+          } else {
             this.loginSuccess()
           }
-        }else{
+        } else {
           this.requestFailed(res)
           this.Logout();
         }
       },
-      departOk(){
-        if(!this.departSelected){
-          this.validate_status='error'
+      departOk() {
+        if (!this.departSelected) {
+          this.validate_status = 'error'
           return false
         }
        let obj = {
-          orgCode:this.departSelected,
-          username:this.form.getFieldValue("username")
+          orgCode: this.departSelected,
+          username: this.form.getFieldValue('username')
         }
-        putAction("/sys/selectDepart",obj).then(res=>{
-          if(res.success){
+        putAction('/sys/selectDepart', obj).then(res => {
+          if (res.success) {
             const userInfo = res.result.userInfo;
             Vue.ls.set(USER_INFO, userInfo, 7 * 24 * 60 * 60 * 1000);
             store.commit('SET_INFO', userInfo);
-            //console.log("---切换组织机构---userInfo-------",store.getters.userInfo.orgCode);
+            // console.log("---切换组织机构---userInfo-------",store.getters.userInfo.orgCode);
             this.departClear()
             this.loginSuccess()
-          }else{
+          } else {
             this.requestFailed(res)
-            this.Logout().then(()=>{
+            this.Logout().then(() => {
               this.departClear()
             });
           }
         })
       },
-      departClear(){
-        this.departList=[]
-        this.departSelected=""
-        this.currentUsername=""
-        this.departVisible=false
-        this.validate_status=''
+      departClear() {
+        this.departList = []
+        this.departSelected = ''
+        this.currentUsername = ''
+        this.departVisible = false
+        this.validate_status = ''
       },
-      departChange(value){
-        this.validate_status='success'
+      departChange(value) {
+        this.validate_status = 'success'
         this.departSelected = value
       },
-    getRouterData(){
+    getRouterData() {
       this.$nextTick(() => {
         if (this.$route.params.username) {
           this.form.setFieldsValue({
@@ -486,23 +488,39 @@
         }
       })
     },
-    //获取密码加密规则
-    getEncrypte(){
+    // 获取密码加密规则
+    getEncrypte() {
       var encryptedString = Vue.ls.get(ENCRYPTED_STRING);
-      if(encryptedString == null){
+      if (encryptedString == null) {
         getEncryptedString().then((data) => {
           this.encryptedString = data
         });
-      }else{
+      } else {
         this.encryptedString = encryptedString;
       }
+    }
     },
+    mounted() {
+      window.WwLogin({
+        id: 'wx_qrcode', // 登录页面显示二维码的容器id
+        appid: 'ww92e8a0cb0975b937', // 企业微信的CorpID，在企业微信管理端查看
+        agentid: '1000002', // 授权方的网页应用id，在具体的网页应用中查看
+        redirect_uri: encodeURIComponent('127.0.0.1'), // 重定向的地址，需要进行encode
+        state: '',
+        href: 'data:text/css;base64,LmltcG93ZXJCb3ggLnFyY29kZSB7d2lkdGg6IDI1MHB4O30NCi5pbXBvd2VyQm94IC50aXRsZSB7ZGlzcGxheTogbm9uZTt9DQouaW1wb3dlckJveCAuaW5mbyB7d2lkdGg6IDIwMHB4O30NCi5zdGF0dXNfaWNvbiB7ZGlzcGxheTogbm9uZSAgIWltcG9ydGFudH0NCi5pbXBvd2VyQm94IC5zdGF0dXMge3RleHQtYWxpZ246IGNlbnRlcjt9'// 自定义样式链接，只支持https协议的资源地址
+      })
     }
   }
 </script>
 
 <style lang="less" scoped>
 
+  .wx_bg{
+    background-color: white;
+    width: 100%;
+    height: 350px;
+    text-align: center;
+  }
   .user-layout-login {
     label {
       font-size: 14px;
